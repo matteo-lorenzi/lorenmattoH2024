224b29c9266f5d8ebc08e96378ffcd41dbe41858
Lolopiz <[lorenzi.matteo30@gmail.com]>
2024-10-29 12:07:49 +0100

    Refactor app routes to redirect to dashboard by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { WeaponsComponent } from './components/weapons/weapons.component';
 import { WeaponDetailComponent } from './components/weapon-detail/weapon-detail.component';
 import { AddHeroeComponent } from './components/add-heroe/add-heroe.component';
 import { AddWeaponComponent } from './components/add-weapon/add-weapon.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'hero-details/:id', component: HeroDetailComponent },
   { path: 'weapons', component: WeaponsComponent },
   { path: 'weapon-details/:id', component: WeaponDetailComponent },
   { path: 'add-heroe', component: AddHeroeComponent },
   { path: 'add-weapon', component: AddWeaponComponent },
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
